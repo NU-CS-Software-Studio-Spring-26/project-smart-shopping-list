d2fd283fc4cef0f695d43cab9cad923f9954a4ec
amiemasih <amiemasih2027@u.northwestern.edu>
2026-05-31 02:26:38 -0500

    Test budget planner category filter (#72)
    Selecting a category scopes the planner to that category; no filter includes products from every category.

---
+++ test/controllers/budget_planner_controller_test.rb
@@ -0,0 +1,28 @@
+require "test_helper"
+
+class BudgetPlannerControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    @user = users(:one)
+    sign_in_as @user
+
+    @earbuds = @user.products.create!(name: "Cheap Earbuds", category: "Electronics")
+    @earbuds.price_records.create!(price: 30, store_name: "S", recorded_at: 1.day.ago)
+
+    @book = @user.products.create!(name: "Paperback Novel", category: "Books")
+    @book.price_records.create!(price: 12, store_name: "S", recorded_at: 1.day.ago)
+  end
+
+  test "category filter limits the planner to the chosen category" do
+    get budget_planner_url(budget: 100, category: "Electronics")
+    assert_response :success
+    assert_match "Cheap Earbuds", response.body
+    assert_no_match(/Paperback Novel/, response.body)
+  end
+
+  test "no category filter includes products from every category" do
+    get budget_planner_url(budget: 100)
+    assert_response :success
+    assert_match "Cheap Earbuds", response.body
+    assert_match "Paperback Novel", response.body
+  end
+end
